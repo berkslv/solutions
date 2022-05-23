804174465a9d02ec93253491c3f1cb242d740b44
berkslv <berkslv@gmail.com>
2022-05-23 14:24:30 +0300

    Added 101. Symmetric Tree

---
+++ leetcode/101-Symmetric-Tree/101-Symmetric-Tree_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/berkslv/algorithms/structures"
+)
+
+type TestCase struct {
+	root     *TreeNode
+	expected bool
+}
+
+var TestCases = []TestCase{
+	{structures.NewTreeNode(1, structures.NewTreeNode(2, structures.NewTreeNode(3, nil, nil), structures.NewTreeNode(4, nil, nil)), structures.NewTreeNode(2, structures.NewTreeNode(4, nil, nil), structures.NewTreeNode(3, nil, nil))), true},
+}
+
+func Test_isSymmetric(t *testing.T) {
+
+	for _, test := range TestCases {
+		if output := isSymmetric(test.root); !reflect.DeepEqual(output, test.expected) {
+			t.Errorf("Output %v not equal to expected %v", output, test.expected)
+		}
+	}
+
+}
